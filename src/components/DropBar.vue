80982a10614e29515047fda4afb5ea2fba6e60c3
Halid Lihovac <halid.lihovac@gmail.com>
2022-10-17 12:09:05 +0200

    fix

--- src/components/DropBar.vue
+++ src/components/DropBar.vue
@@ -1,50 +1,38 @@
 <template>
-
-		<select
-			name="Any"
-			id="any"
-			v-model="selected"
-			@change="translatePage"
-		>
-			<option 
-				v-for="lang in fetchDropData"
-				:value="lang.abr"
-			>
-				{{ lang.langauge }}
-			</option>
-		</select>
-
+  <select name="Any" id="any" v-model="selected" @change="translatePage">
+    <option v-for="lang in fetchDropData" :value="lang.abr">
+      {{ lang.langauge }}
+    </option>
+  </select>
 </template>
 
 <script>
-	export default {
-		name:'DropBar',
-		data() {
-			return {
-				selected: 'bs',
-				languages: [
-					{ langauge: 'English', abr: 'en' },
-					{ langauge: 'Bosanski', abr: 'bs' },
-					{ langauge: 'Njemački', abr: 'de' },
-				],
-			};
-		},
-		computed: {
-			fetchDropData() {
-				return this.languages;
-			},
-		},
-		methods: {
-			translatePage() {
-				return (this.$i18n.locale = this.selected);
-			},
-		},
-	};
+export default {
+  name: "DropBar",
+  data() {
+    return {
+      selected: "bs",
+      languages: [
+        { langauge: "English", abr: "en" },
+        { langauge: "Bosanski", abr: "bs" },
+        { langauge: "Njemački", abr: "de" },
+      ],
+    };
+  },
+  computed: {
+    fetchDropData() {
+      return this.languages;
+    },
+  },
+  methods: {
+    translatePage() {
+      return (this.$i18n.locale = this.selected);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
-select{
-
+select {
 }
 </style>
