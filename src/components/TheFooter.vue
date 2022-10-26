8a0b88ccc84d974b59b6c570045035c35788a510
Halid Lihovac <halid.lihovac@gmail.com>
2022-10-26 14:33:33 +0200

    update

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -6,17 +6,12 @@
     data-aos-anchor-placement="center-bottom"
   >
     <footer class="text-center text-lg-start bg-light text-muted">
-      <!-- Section: Social media -->
       <section
         class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
       >
-        <!-- Left -->
         <div class="me-5 d-none d-lg-block">
           <span>Get connected with us on social networks:</span>
         </div>
-        <!-- Left -->
-
-        <!-- Right -->
         <div>
           <a
             href="https://www.facebook.com/Forspace-Solutions-104570971684748"
@@ -40,33 +35,24 @@
             <font-awesome-icon icon="fa-brands fa-linkedin" />
           </a>
         </div>
-        <!-- Right -->
       </section>
-      <!-- Section: Social media -->
-
-      <!-- Section: Links  -->
       <section class="">
         <div class="container text-center text-md-start mt-5">
-          <!-- Grid row -->
+
           <div class="row mt-3">
-            <!-- Grid column -->
             <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-              <!-- Content --> <img src="../assets/svg/logo.svg" alt="">
-              <h6 class="text-uppercase fw-bold mb-4">
-              
-              </h6>
-              <p class="ms-5"> 
-               Mi ne radimo sa vama!
-               <br>
-               Mi radimo za vas!
+              <img src="../assets/svg/logo.svg" alt="" />
+              <h6 class="text-uppercase fw-bold mb-4"></h6>
+              <p class="ms-5">
+                Mi ne radimo sa vama!
+                <br />
+                Mi radimo za vas!
               </p>
             </div>
-            <!-- Grid column -->
-
-            <!-- Grid column -->
             <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-              <!-- Links -->
-              <h6 class="text-uppercase fw-bold mb-4">Products</h6>
+              <RouterLink to="/products"
+                ><h6 class="text-uppercase fw-bold mb-4">Products</h6>
+              </RouterLink>
               <p>
                 <a href="#!" class="text-reset">Računovodstvo</a>
               </p>
@@ -79,46 +65,31 @@
               <p>
                 <a href="#!" class="text-reset">Mobilne aplikacije</a>
               </p>
-       
             </div>
-            <!-- Grid column -->
-
-            <!-- Grid column -->
-         
-            <!-- Grid column -->
-
-            <!-- Grid column -->
             <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-              <!-- Links -->
               <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
               <p>
-                <font-awesome-icon icon="fa-solid fa-home" /> 
-                Bulevar Meše Selimovića 16,
-                71000 Sarajevo, Bosna i Hercegovina
+                <font-awesome-icon icon="fa-solid fa-home" />
+                Bulevar Meše Selimovića 16, 71000 Sarajevo, Bosna i Hercegovina
               </p>
               <p>
                 <font-awesome-icon icon="fa-solid fa-envelope" />
                 info@forspace.ba
               </p>
-              <p> <font-awesome-icon icon="fa-solid fa-phone" /> +387 33 774 873</p>
+              <p>
+                <font-awesome-icon icon="fa-solid fa-phone" /> +387 33 774 873
+              </p>
             </div>
-            <!-- Grid column -->
           </div>
-          <!-- Grid row -->
         </div>
       </section>
-      <!-- Section: Links  -->
-
-      <!-- Copyright -->
       <div
         class="text-center p-2"
         style="background-color: rgba(0, 0, 0, 0.05)"
       >
         © 2022 Forspace - All Rights Reserved
       </div>
-      <!-- Copyright -->
     </footer>
-    <!-- Footer -->
   </div>
 </template>
 <script>
@@ -128,6 +99,7 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-*{
-  color:#263b53;
-}</style>
+* {
+  color: #263b53;
+}
+</style>
