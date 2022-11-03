5d31e4fb286f10598a3c35b2ddc5398dd2a4ef08
Halid Lihovac <halid.lihovac@gmail.com>
2022-11-03 10:15:44 +0100

    update

--- src/components/services/ServicesSlider.vue
+++ src/components/services/ServicesSlider.vue
@@ -3,13 +3,16 @@
   <Carousel :wrap-around="true" :items-to-show="1">
     <Slide v-for="slide in images" :key="slide.id">
       <div class="carousel__item">
-        <h1>{{slide.name}}</h1>
+        <h1>{{ slide.name }}</h1>
         <img
+          @mousedown="pressedMouse = true"
+          @mouseup="pressedMouse = false"
+          :class="{ grabbed: pressedMouse }"
           @mousemove="setCurrentSlide(slide.id)"
           :src="slide.url"
           alt="logo"
           :id="slide.id"
-        />
+        /><Navigation data-aos="fade" data-aos-delay="800"></Navigation>
       </div>
     </Slide>
     <template #addons>
@@ -19,7 +22,7 @@
 </template>
 
 <script>
-import { Carousel, Pagination, Slide } from "vue3-carousel";
+import { Carousel, Pagination, Slide, Navigation } from "vue3-carousel";
 
 import "vue3-carousel/dist/carousel.css";
 
@@ -30,10 +33,12 @@ export default {
     Carousel,
     Slide,
     Pagination,
+    Navigation
   },
   data() {
     return {
       currentSlide: 1,
+      pressedMouse: false,
     };
   },
   methods: {
@@ -44,28 +49,31 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-h1{
-    padding:7px;
-    position: absolute;
-    left:20%;
-    top:20%;
-    background-color: rgba(255, 253, 253, 0.5);
-    
+h1 {
+  padding: 7px;
+  position: absolute;
+  left: 20%;
+  top: 20%;
+  background-color: rgba(255, 253, 253, 0.5);
 }
 .carousel__pagination {
   z-index: 21;
   position: absolute;
-  bottom: 10%;
+  bottom: 15%;
   width: 100%;
   border-radius: 50%;
   li {
+    background-color: white!important;
     button {
       border-radius: 50%;
     }
   }
 }
 .wrapper {
-  height: 90vh;
+  height: 80vh;
+}
+.grabbed {
+  cursor: grabbing !important;
 }
 .carousel__item {
   height: 100vh;
@@ -74,14 +82,13 @@ h1{
   font-size: 20px;
   border-radius: 8px;
   display: flex;
-  &:hover {
-    cursor: pointer;
-  }
+
   img {
     background-size: contain;
     object-fit: cover;
     width: 100%;
-    height: 90vh;
+    height: 80vh;
+    cursor: grab;
   }
 }
 
