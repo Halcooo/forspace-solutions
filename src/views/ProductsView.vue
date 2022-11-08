7e844de8ea4002ec0ff2a2d0ef354e9786780439
Halid Lihovac <halid.lihovac@gmail.com>
2022-11-08 12:20:24 +0100

    removes clases

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -2,25 +2,25 @@
 	<div class="wrapper">
 		<div class="container items">
 			<div class="row">
-				<div class="col-sm">
-					<div class="card nabava">nabava</div>
+				<div class="">
+					<div class="nabava">nabava</div>
 				</div>
-				<div class="col-sm">
-					<div class="card prodaja">prodaja</div>
+				<div class="">
+					<div class="prodaja">prodaja</div>
 				</div>
-				<div class="col-sm">
-					<div class="card proizvodnja">proizvodnja</div>
+				<div class="">
+					<div class="proizvodnja">proizvodnja</div>
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-sm">
-					<div class="card skladište">skladište</div>
+				<div class="">
+					<div class="skladište">skladište</div>
 				</div>
-				<div class="col-sm">
-					<div class="card dms">dms</div>
+				<div class="">
+					<div class=" dms">dms</div>
 				</div>
-				<div class="col-sm">
-					<div class="card finansije">finansije</div>
+				<div class="">
+					<div class="finansije">finansije</div>
 				</div>
 			</div>
 		</div>
