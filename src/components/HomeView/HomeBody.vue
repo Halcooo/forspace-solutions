5a3f7ddf3c8ddedaf50808f475e4abec886a4791
Amirceljo <amir.celjo2@gmail.com>
2022-11-08 08:35:16 +0100

    update

--- src/components/HomeView/HomeBody.vue
+++ src/components/HomeView/HomeBody.vue
@@ -69,6 +69,31 @@
 	</div>
 </template>
 <script>
+	import BaseSlider from '../BaseSlider.vue';
+	export default {
+		name: 'HomeBody',
+		components: {
+			BaseSlider,
+		},
+		data() {
+			return {
+				images: [
+					{ id: 1, url: require('@/assets/images/asa-group-logo.png') },
+					{ id: 2, url: require('@/assets/images/prevent-logo.png') },
+					{ id: 3, url: require('@/assets/images/interior-logo-2.png') },
+					{ id: 4, url: require('@/assets/images/salona-logo.png') },
+					{ id: 5, url: require('@/assets/images/dresscode-logo.png') },
+					{ id: 6, url: require('@/assets/images/tkt-logo.png') },
+					{ id: 7, url: require('@/assets/images/Savent.png') },
+					{ id: 9, url: require('@/assets/images/Maxuz.png') },
+					{ id: 10, url: require('@/assets/images/Itrade.png') },
+					{ id: 11, url: require('@/assets/images/Gradiz.jpg') },
+					{ id: 12, url: require('@/assets/images/Putovaobih.png') },
+					{ id: 13, url: require('@/assets/images/ApDigital.jpg') },
+				],
+			};
+		},
+	};
 import BaseSlider from "../BaseSlider.vue";
 export default {
   name: "HomeBody",
