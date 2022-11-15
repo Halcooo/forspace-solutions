8d1ffa21b35a80abb9b3e3a590126fce129d718a
Amirceljo <amir.celjo2@gmail.com>
2022-11-15 10:05:38 +0100

    dgsdfgsd

--- src/components/Forms/buttons/ClosingButton.vue
+++ src/components/Forms/buttons/ClosingButton.vue
@@ -1,70 +1,64 @@
 <template>
-	<div class="button_device">
-		<div>
-			<p
-				class="x-one"
-				:class="{ btn: btn }"
-			></p>
-			<p
-				class="x-two"
-				:class="{ btn: btn }"
-			></p>
-		</div>
-	</div>
+  <div class="button_device">
+    <div>
+      <p class="x-one" :class="{ btn: btn }"></p>
+      <p class="x-two" :class="{ btn: btn }"></p>
+    </div>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: 'ClosingButton',
-		props: ['btn'],
-	};
+export default {
+  name: "ClosingButton",
+  props: ["btn", "map"],
+};
 </script>
 
 <style lang="scss" scoped>
-	.button_device {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		display: block;
-		&:hover {
-			cursor: pointer;
-		}
-		div {
-			position: relative;
-			width: 100%;
-			height: 100%;
-		}
-		.x-one {
-			width: 100%;
-			padding: 1px;
-			border-radius: 50%;
-			background-color: orangered;
-			transform: rotate(45deg);
-			position: absolute;
-			top: 50%;
-			left: 0;
-		}
-		.x-two {
-			width: 100%;
-			padding: 1px;
-			border-radius: 50%;
-			background-color: orangered;
-			transform: rotate(-45deg);
-			position: absolute;
-			top: 50%;
-			left: 0;
-		}
-		@media screen and (max-width: 560px) {
-			.sidenav-content {
-				.button_device {
-					left: 88%;
-				}
-			}
-		}
+.button_device {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: block;
+  &:hover {
+    cursor: pointer;
+  }
+  div {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+  .x-one {
+    width: 100%;
+    padding: 1px;
+    border-radius: 50%;
+    background-color: orangered;
+    transform: rotate(45deg);
+    position: absolute;
+    top: 50%;
+    left: 0;
+  }
+  .x-two {
+    width: 100%;
+    padding: 1px;
+    border-radius: 50%;
+    background-color: orangered;
+    transform: rotate(-45deg);
+    position: absolute;
+    top: 50%;
+    left: 0;
+  }
+  @media screen and (max-width: 560px) {
+    .sidenav-content {
+      .button_device {
+        left: 88%;
+      }
+    }
+  }
 
-		.btn {
-			background-color: rgb(255, 255, 255);
-			border-radius: 50px;
-		}
-	}
+  .btn {
+    background-color: rgb(255, 255, 255);
+    border-radius: 50px;
+  }
+}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,23 @@
-import { createStore } from 'vuex';
-import servicesModule from './modules/services.js';
+import { createStore } from "vuex";
+import servicesModule from "./modules/services.js";
 
 export default createStore({
-	state: {
-		none: true,
-		sidenav: false,
-		sidecontent: false,
-	},
-	getters: {
-		getSideNav(state) {
-			return state.sidenav;
-		},
-	},
-	mutations: {
-		setSideNav(state, payload) {
-			state.sidenav = payload;
-		},
-	},
-	actions: {},
-	modules: { servicesModule },
+  state: {
+    none: true,
+    sidenav: false,
+    sidecontent: false,
+    map: false,
+  },
+  getters: {
+    getSideNav(state) {
+      return state.sidenav;
+    },
+  },
+  mutations: {
+    setSideNav(state, payload) {
+      state.sidenav = payload;
+    },
+  },
+  actions: {},
+  modules: { servicesModule },
 });
