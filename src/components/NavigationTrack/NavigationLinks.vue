f113c414f8db46b0104c2174e3f2c7ced7360838
Amirceljo <amir.celjo2@gmail.com>
2022-12-01 12:20:19 +0100

    fdgsdf

--- src/components/NavigationTrack/NavigationLinks.vue
+++ src/components/NavigationTrack/NavigationLinks.vue
@@ -15,12 +15,7 @@
       <div class="underline"></div>
     </router-link>
   </li>
-  <!-- <li>
-    <div class="search ms-5 text-light">
-      <font-awesome-icon icon="fa-solid fa-search" />
-    </div>
-  </li> -->
-  <!-- <li>
+  <li>
     <div class="box ms-3" @mouseover="showLanguages">
       {{ language }}
       <font-awesome-icon icon="fa-solid fa-chevron-down" />
@@ -31,7 +26,7 @@
         </p>
       </div>
     </div>
-  </li> -->
+  </li>
 </template>
 
 <script>
